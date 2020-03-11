71fb80612e260e8c40442640f084ee893d621d23
KuaiYu95 <kuaiyumobile@gmail.com>
2020-03-11 23:42:36 +0800

    v2.0.0

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.575af23ac2deefa229fac7915a18fb6f.js"
+  "/precache-manifest.ab030f332d6682050bfec94aaf0dbf99.js"
 );
 
 self.addEventListener('message', (event) => {
